3f478b1d67155ae13818ec75c72d209000b89b39
DEAT Nicolas <74407424+KajinnD@users.noreply.github.com>
2021-12-05 18:35:42 +0100

    Add files via upload

--- mainGPU.cu
+++ mainGPU.cu
@@ -103,7 +103,7 @@ __global__ void searchPrimesGPU(ULONGLONG * inData, bool * listePrime, ULONGLONG
           //cell prime;
           //prime.nbPrime = inData[tid];
           outData[*j] = inData[tid];
-          atomicAdd(*j, (ULONGLONG)1);
+          //atomicAdd(*j, (ULONGLONG)1);
       }
       tid += blockDim.x * gridDim.x;
   }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 all: test
 
 test:
-   nvcc -arch=compute_50 -o prime main.cu prime.cpp prime.hpp -Xcompiler -fopenmp
+   nvcc -arch=compute_50 -o prime main.cu prime.cpp -Xcompiler -fopenmp
 
 clean:
 		rm -rf *.o
