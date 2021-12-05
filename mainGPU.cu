67bd083587ad8887a9cb28b6218f2a7953262272
DEAT Nicolas <74407424+KajinnD@users.noreply.github.com>
2021-12-05 14:37:41 +0100

    Add files via upload

--- mainGPU.cu
+++ mainGPU.cu
@@ -164,8 +164,7 @@ int main(int argc, char *argv[]) {
       inData[j] = i;
       cudaMemcpy(dev_inData, inData, sqrt(n) * sizeof(ULONGLONG), cudaMemcpyHostToDevice);
       cudaMemcpy(dev_isPrime, &isPrime, sizeof(bool), cudaMemcpyHostToDevice);
-      v0_isPrimeGPU<<<5, 16>>>(dev_inData, dev_n, dev_isPrime);
-      printf("fin\n");
+      v0_isPrimeGPU<<<n, 1>>>(dev_inData, dev_n, dev_isPrime);
       cudaMemcpy(&isPrime, dev_isPrime, sizeof(bool), cudaMemcpyDeviceToHost);
       listPrime[j] = isPrime;
       j++;
